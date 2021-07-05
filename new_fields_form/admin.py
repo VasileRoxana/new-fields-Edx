bc1e337ed9bf2dc57758f4fc826601df08cb361a
Rox <roxana.vasile@softbinator.com>
2021-07-06 00:11:35 +0300

    changed ExtraInfo model name

--- new_fields_form/admin.py
+++ new_fields_form/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import ExtraInfo
+from .models import ExtraFields
 import csv
 from django.http import HttpResponse
 from io import StringIO
 
 
-class ExtraInfoAdmin(admin.ModelAdmin):
+class ExtraFieldsAdmin(admin.ModelAdmin):
     actions = ['download_csv'] 
     list_display = ('user', 'age', 'birth_month', 'sex', 'studies', 'job', 'expectations', 'user_environment')
     list_display_links = ('user', 'age', 'birth_month', 'sex', 'studies', 'job', 'expectations', 'user_environment',)
--- new_fields_form/forms.py
+++ new_fields_form/forms.py
@@ -4,12 +4,12 @@ from django.utils.translation import gettext_lazy as _
 import logging
 
 
-class ExtraInfoForm(ModelForm):
+class ExtraFieldsForm(ModelForm):
     """
     The fields on this form are derived from the ExtraInfo model in models.py.
     """
     def __init__(self, *args, **kwargs):
-        super(ExtraInfoForm, self).__init__(*args, **kwargs)
+        super(ExtraFieldsForm, self).__init__(*args, **kwargs)
         self.fields['age'].required = True
         self.fields['birth_month'].required = True
         self.fields['sex'].required = True
@@ -20,7 +20,7 @@ class ExtraInfoForm(ModelForm):
         
 
     class Meta(object):
-        model = ExtraInfo
+        model = ExtraFields
         fields = ('age', 'birth_month', 'sex','studies','job','expectations','user_environment')
         labels = {'age': _("Varsta"), 'birth_month': _("Luna nasterii"), 'sex': _("Sex"),'job': _("Job"),'studies': _("Studii"),'expectations': _("Asteptari in urma absolvirii"),'user_environment': _("Mediu")}
         help_text = {'age': _("Te rugam selectează o optiune"), 'birth_month': _("Te rugam selectează o optiune"), 'sex': _("Te rugam selectează o optiune"),'studies': _("Te rugam selectează o optiune"),'expectations': _("Te rugam selectează o optiune"), 'user_environment': _("Te rugam selectează o optiune")}

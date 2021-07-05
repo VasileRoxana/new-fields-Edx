9b902be774fed9b8c8af70408b09e539fe45dd67
Rox <roxana.vasile@softbinator.com>
2021-07-06 00:21:02 +0300

    changed model name in model.py

--- new_fields_form/models.py
+++ new_fields_form/models.py
@@ -5,7 +5,7 @@ from django.db import models
 USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
-class ExtraInfo2(models.Model):
+class ExtraFields(models.Model):
     """
     This model contains two extra fields that will be saved when a user registers.
     The form that wraps this model is in the forms.py file.
